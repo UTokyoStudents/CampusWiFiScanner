68302a1a72ec1fe50f7adfd894edd31e826db05e
Masora <masora@menhera.org>
2021-02-16 12:41:45 +0900

    updated sdk

--- app/src/main/java/org/menhera/campus_wifi/CampusWiFiApplication.java
+++ app/src/main/java/org/menhera/campus_wifi/CampusWiFiApplication.java
@@ -16,6 +16,7 @@ import android.net.wifi.WifiManager;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -57,7 +58,7 @@ public class CampusWiFiApplication extends Application {
     FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
-    public void onCreate(){
+    public void onCreate() {
         super.onCreate();
 
         wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
@@ -75,6 +76,16 @@ public class CampusWiFiApplication extends Application {
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location aLocation) {
